3482f83446cc74210254bb1b41c14df05cba78f6
Qing Wang <chingwang.qw@gmail.com>
2019-09-18 19:19:10 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -15,10 +15,11 @@ def find_max_value(array)
   max_num = 0 
   while counter < array.length
   if array[counter] > max_num
-  return max_num 
+    max_num = array[counter]
   end
   counter += 1 
 end
+return max_num
 end 
 
 def find_min_value(array)
